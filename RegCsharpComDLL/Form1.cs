7ac297e43dfe836bc7ae25581cc6bc6ae29c322a
jiaolong <zzia615@163.com>
2020-07-27 19:50:16 +0800

    立项

---
+++ RegCsharpComDLL/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RegCsharpComDLL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Text = ".net40";
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "全部文件（*.*）|*.*";
+            openFileDialog1.FileName = string.Empty;
+            openFileDialog1.FileOk += (ss, ee) =>
+            {
+                textBox2.Text = openFileDialog1.FileName;
+            };
+            openFileDialog1.ShowDialog();
+        }
+        void RunCmd(string file,string args)
+        {
+
+
+            string s_regasm = file;
+
+            ProcessStartInfo startInfo = new ProcessStartInfo(s_regasm);
+            startInfo.Arguments = args;
+            startInfo.StandardOutputEncoding = Encoding.Default;
+            startInfo.StandardErrorEncoding = Encoding.Default;
+            startInfo.RedirectStandardOutput = true;
+            startInfo.RedirectStandardError = true;
+            startInfo.CreateNoWindow = true;
+            startInfo.UseShellExecute = false;
+            Process process = Process.Start(startInfo);
+            process.WaitForExit();
+            textBox1.Text += process.StandardOutput.ReadToEnd();
+            textBox1.Text += process.StandardError.ReadToEnd();
+        }
+        string GetPath()
+        {
+            string path = @"C:\Windows\Microsoft.NET\Framework\v4.0.30319\";
+            if (comboBox1.Text == ".net40")
+            {
+                if (!Directory.Exists(path))
+                {
+                    MessageBox.Show("未安装.net framework 4.0环境");
+                    return "";
+                }
+            }
+            else
+            {
+                path = @"C:\Windows\Microsoft.NET\Framework\v2.0.50727\";
+
+                if (!Directory.Exists(path))
+                {
+                    MessageBox.Show("未安装.net framework 2.0环境");
+                    return "";
+                }
+            }
+            return path;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string path = GetPath();
+            if (string.IsNullOrEmpty(path))
+            {
+                return;
+            }
+
+            string s_regasm = Path.Combine(path, "regasm.exe");
+            RunCmd(s_regasm, "/codebase " + textBox2.Text);
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path = GetPath();
+            if (string.IsNullOrEmpty(path))
+            {
+                return;
+            }
+
+            string s_regasm = Path.Combine(path, "regasm.exe");
+            RunCmd(s_regasm, "/codebase " + textBox2.Text+" /u");
+        }
+    }
+}
